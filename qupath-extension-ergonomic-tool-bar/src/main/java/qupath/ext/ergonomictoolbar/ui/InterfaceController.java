1b238579fa2252ad4e88f03a96c62e1f06e73973
Baptiste <batinouq@gmail.com>
2024-04-12 10:13:39 +0200

    Rebase de Baptiste à partir de master

--- qupath-extension-ergonomic-tool-bar/src/main/java/qupath/ext/ergonomictoolbar/ui/InterfaceController.java
+++ qupath-extension-ergonomic-tool-bar/src/main/java/qupath/ext/ergonomictoolbar/ui/InterfaceController.java
@@ -1,27 +1,44 @@
 package qupath.ext.ergonomictoolbar.ui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javafx.stage.Stage;
 import qupath.ext.ergonomictoolbar.ErgonomicToolBarExtension;
 import qupath.fx.dialogs.Dialogs;
 import qupath.lib.gui.QuPathGUI;
 import qupath.lib.gui.viewer.QuPathViewer;
+import qupath.lib.gui.QuPathGUI;
+import qupath.lib.gui.measure.ObservableMeasurementTableData;
+import qupath.lib.images.ImageData;
+import qupath.lib.objects.PathObject;
+import qupath.lib.objects.hierarchy.events.PathObjectSelectionListener;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.ResourceBundle;
+
+import static java.lang.Math.round;
+import static qupath.lib.gui.scripting.QPEx.getQuPath;
+import static qupath.lib.scripting.QP.*;
 
 /**
  * Controller for UI pane contained in interface.fxml
  */
 
-public class InterfaceController extends VBox {
+public class InterfaceController extends VBox implements PathObjectSelectionListener{
 
     private boolean is_Names_Display = true;
 
@@ -111,5 +128,28 @@ public class InterfaceController extends VBox {
         QuPathGUI quPath_GUI = QuPathGUI.getInstance();
         QuPathViewer viewer = quPath_GUI.getViewer();
         viewer.getOverlayOptions().setShowNames(is_Names_Display);
+    }
+
+    public void selectedPathObjectChanged(PathObject pathObjectSelected, PathObject previousObject, Collection<PathObject> allSelected) {
+        // Here goes your selection change logic
+        //mettre dans le constructeur
+        //qupath = getQuPath();
+        //         qupath.getImageData().getHierarchy().getSelectionModel().addPathObjectSelectionListener(this);
+        if (pathObjectSelected == null){
+            //label.setText("No selection");
+            System.out.println("No selection");
+        }
+        else {
+            ImageData imageData = getCurrentImageData();
+
+            Collection<PathObject> tissues = getAnnotationObjects();
+            ObservableMeasurementTableData ob = new ObservableMeasurementTableData();
+            ob.setImageData(imageData, tissues);
+            String area = "Area µm^2";
+            double annotationArea = ob.getNumericValue(getCurrentHierarchy().getSelectionModel().getSelectedObject(), area);
+            //label.setText("Names: " + (annotationArea/1000000) + "\n");
+            double roundArea = (double) round(annotationArea/1000)*1000;
+            System.out.println("Aires: " + roundArea/1000000 + "\n");
+        }
     }
 }
